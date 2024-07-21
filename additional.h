a9d99bcf5efa695e8cae0a944131a0c6159b340b
Boopalan0 <163556463+Boopalan0@users.noreply.github.com>
2024-07-21 13:35:06 +0530

    Add files via upload
    additional header files

---
+++ additional.h
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void start(){
+	printf("\n<-------------- program starts -------------->\n");
+}
+void println(){
+	printf("============================================>>\n");
+}
+void printsln(){
+	printf("-------------------------------------------->>\n");
+}
+void ar(){
+	printf("-> ");
+}
+void tar(){
+	printf("\t-> ");
+}
+void hd(){
+	printf("\n\t");
+}
+void uhd(){
+	printf("====================================\n");
+}
+void end(){
+	printf("\n<-------------- program ended --------------->\n\n");
+}
+
